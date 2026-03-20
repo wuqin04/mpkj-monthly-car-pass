c50fcd9071ec9d88b140ff68336ce6c3ec217aa6
KHOO XQ <xianqikhoo190477@gmail.com>
2026-03-20 16:59:09 +0800

    paymentHistory

--- functions/admin/paymentHistory.cpp
+++ functions/admin/paymentHistory.cpp
@@ -13,17 +13,18 @@ void paymentHistoryMenu() {
         string name = "user";   //later will be replaced by actual user name from transaction history data
         double transaction_amount = 100.00; //later will be replaced by actual transaction amount from transaction history data
         string date = "2023-10-01";     //later will be replaced by actual transaction date from transaction history data
-        cout<<"============================================\n";
-        cout<<"            PAYMENT HISTORY                 \n";
-        cout<<"============================================\n";
-        cout<<"|  No. | User ID | Amount Paid        | Date       |\n";
+        cout<<"=================================================\n";
+        cout<<"                 PAYMENT HISTORY                 \n";
+        cout<<"=================================================\n";
+        cout<<"=================================================\n";
+        cout<<"|  No. | User ID | Amount Paid    |    Date     |\n";
         for(int i=0; i<n; i++){
-        cout<<"| "<<setw(2)<<i+1<<"   |"<<fixed<<left<<setw(8)<<name<<" | RM("<<fixed<<left<<setw(7)<<transaction_amount<<")| "<<date<<" |\n";
+        cout<<"| "<<setw(2)<<i+1<<"   | "<<fixed<<left<<setw(8)<<setprecision(2)<<name<<"| RM "<<fixed<<left<<setw(7)<<transaction_amount<<"     | "<<date<<"  |\n";
         }
-        cout<<"============================================\n";
-        cout<<"| (1) View payment                         |\n";
-        cout<<"| (2) Back to admin menu                   |\n";
-        cout<<"============================================\n";
+        cout<<"=================================================\n";
+        cout<<"| (1) View payment                              |\n";
+        cout<<"| (2) Back to admin menu                        |\n";
+        cout<<"=================================================\n";
         int choice;
         cout << "Choose an option: ";
         if (!(cin >> choice)) {
